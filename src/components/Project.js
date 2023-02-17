bff2e1a5c70b6f14b6d1cb6c65d5ad51833471f1
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-02-17 15:31:23 -0500

    added project containers

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,8 +7,26 @@ export default function Project() {
             <h2 class="text-center">Projects:</h2>
             <Row>
                 <Col xs={12} md={6} lg={4}>
-                    <a href="https://enigmatic-tundra-51864.herokuapp.com/" target="_"><img src={projectTitle} className='img-fluid rounded' alt='Project' /></a>
-                    <h2 class="text-info">Project Title</h2>
+                    <a href="https://stormy-tundra-00611.herokuapp.com/" target="_"><img src={projectTitle} className='img-fluid rounded' alt='Project' /></a>
+                    <h2 class="text-info">PetBook</h2>
+                    <p>Lorem Ipsum</p>
+                    <p><a class="btn btn-secondary" target="_" href="repositoryURL" role="button">Repository »</a> <a class="btn btn-primary" target="_" href="deployedPage" role="button">Deployment »</a></p>
+                </Col>
+                <Col xs={12} md={6} lg={4}>
+                    <a href="https://carsondipan.github.io/movie-search-project/" target="_"><img src={projectTitle} className='img-fluid rounded' alt='Project' /></a>
+                    <h2 class="text-info">Movie Search</h2>
+                    <p>Lorem Ipsum</p>
+                    <p><a class="btn btn-secondary" target="_" href="repositoryURL" role="button">Repository »</a> <a class="btn btn-primary" target="_" href="deployedPage" role="button">Deployment »</a></p>
+                </Col>
+                <Col xs={12} md={6} lg={4}>
+                    <a href="https://carsondipan.github.io/Schedule-Planner/" target="_"><img src={projectTitle} className='img-fluid rounded' alt='Project' /></a>
+                    <h2 class="text-info">Schedule Planner</h2>
+                    <p>Lorem Ipsum</p>
+                    <p><a class="btn btn-secondary" target="_" href="repositoryURL" role="button">Repository »</a> <a class="btn btn-primary" target="_" href="deployedPage" role="button">Deployment »</a></p>
+                </Col>
+                <Col xs={12} md={6} lg={4}>
+                    <a href="https://carsondipan.github.io/weather-dashboard/" target="_"><img src={projectTitle} className='img-fluid rounded' alt='Project' /></a>
+                    <h2 class="text-info">Weather Dashboard</h2>
                     <p>Lorem Ipsum</p>
                     <p><a class="btn btn-secondary" target="_" href="repositoryURL" role="button">Repository »</a> <a class="btn btn-primary" target="_" href="deployedPage" role="button">Deployment »</a></p>
                 </Col>
--- src/components/projectContainer.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import Nav from './navigation';
-import Info from './info';
-import Project from './project';
-import Footer from './footer'
-
-export default function projectContainer() {
-    // const 
-};
