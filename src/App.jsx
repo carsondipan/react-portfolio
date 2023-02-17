8cc26b421c24445c3687f44458419fd0a87557cb
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-02-17 12:00:04 -0500

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
-
-  return (
-    <nav>
-      <Navbar bg="dark" variant="dark">
-        <Container fluid>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </nav>
-
-  )
-}
+    return (
+          <Navbar bg="dark" variant="dark">
+            <Container fluid>
+              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+    )
+};
 
 export default App
