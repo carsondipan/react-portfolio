440700bcdec4be27e1f704635ba48bc196ed3dd9
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-02-17 19:23:51 -0500

    tried to reconfig with vite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 // Import React Router Link component for internal hyperlinks
-import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
       <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
         {/* Use Link component to create a link that returns to the homepage on click */}
-        <Link className="text-dark" to="/">
-          <h1 className="m-0" style={{ fontSize: '3rem' }}>
-            Home
-          </h1>
-        </Link>
-        <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
+        <p className="m-0">
           Carson Di Pangrazio
         </p>
       </div>
